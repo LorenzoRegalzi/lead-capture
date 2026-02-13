4d0a923522ea149b4d3e028181aa6e6546c693c7
Lorenzo Regalzi <lorenzo.regalzi@gmail.com>
2026-02-13 15:39:28 +0100

    test

--- app/components/MainContentComponent.tsx
+++ app/components/MainContentComponent.tsx
@@ -57,13 +57,13 @@ export default function MainContentComponent({
               {barcodes.map((item, idx) => (
                 <li
                   key={item.barcode + idx}
-                  className="flex justify-between items-center py-1 text-black"
+                  className="flex justify-between items-center py-1 text-black mb-2"
                   style={{ height: '60px' }}
                 >
                   <span className="font-mono">{item.barcode}</span>
-                  <div className="flex items-center gap-2">
+                  <div className="flex items-center gap-2 ">
                     <button
-                      className="bg-blue-950 text-white rounded-full w-10 h-10 flex items-center justify-center text-2xl font-bold transition hover:bg-blue-700 active:bg-blue-800"
+                      className="bg-blue-950 text-white rounded-full w-10 h-10 flex items-center justify-center text-2xl font-bold transition hover:bg-blue-700 active:bg-blue-800 rounded-button"
                       onClick={() => increment(idx)}
                       aria-label="Increment"
                     >
@@ -71,7 +71,7 @@ export default function MainContentComponent({
                     </button>
                     <span className="text-lg font-semibold">x{item.quantity}</span>
                     <button
-                      className="bg-gray-300 text-black rounded-full w-10 h-10  flex items-center justify-center text-2xl font-bold transition hover:bg-gray-400 active:bg-gray-500"
+                      className="bg-gray-300 text-black rounded-full w-10 h-10  flex items-center justify-center text-2xl font-bold transition hover:bg-gray-400 active:bg-gray-500 rounded-button"
                       onClick={() => decrement(idx)}
                       aria-label="Decrement"
                     >
